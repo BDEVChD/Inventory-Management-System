a684aea83d24b266cf2dca7d0145b7a0030632a3
Bladi Duran <bladi.duran@gmail.com>
2021-02-16 16:03:39 -0500

    restructured menu pages on dashboard

--- database/migrations/1613060887046_create_products_schema.js
+++ database/migrations/1613060887046_create_products_schema.js
@@ -7,14 +7,15 @@ class CreateProductsSchema extends Schema {
   up(){
     this.raw(
       `CREATE TABLE products(
-      id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+      id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
       title VARCHAR(200) NOT NULL, 
       sku VARCHAR(60) NOT NULL, 
       material VARCHAR(60) NOT NULL, 
       description TEXT NOT NULL,
       brand_id INT UNSIGNED, 
-      qty INT UNSIGNED, 
-      size FLOAT UNSIGNED, 
+      qty INT UNSIGNED NOT NULL, 
+      size FLOAT UNSIGNED NOT NULL, 
+      user_id INT UNSIGNED NOT NULL,
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
       
@@ -25,7 +26,7 @@ class CreateProductsSchema extends Schema {
 
   down () {
     this.raw()
-    `this.drop('create_products')`
+    `DROP TABLE products`
   }
 }
 
--- database/migrations/1613064478071_create_orders_schema.js
+++ database/migrations/1613064478071_create_orders_schema.js
@@ -4,11 +4,11 @@
 const Schema = use('Schema')
 
 class CreateOrdersSchema extends Schema {
-  up(){
+  up () {
     this.raw(
-        `CREATE TABLE orders(
-        id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-        f_name VARCHAR(200) NOT NULL, 
+      `CREATE TABLE orders(
+        id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        f_name VARCHAR(200) NOT NULL,
         l_name VARCHAR(60) NOT NULL,
         address VARCHAR(100) NOT NULL,
         address_2 VARCHAR(100),
@@ -16,19 +16,14 @@ class CreateOrdersSchema extends Schema {
         state VARCHAR(60) NOT NULL,
         country VARCHAR(3) NOT NULL DEFAULT 'USA',
         payment_type VARCHAR(60) NOT NULL DEFAULT 'paypal',
-        user_id INT NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+        user_id INT UNSIGNED NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-        
-        
-        )`)
+      )`)
   }
 
   down () {
-    this.raw(`
-      DROP TABLE ORDERS
-       `)
+    this.raw(`DROP TABLE ORDERS`)
   }
 }
-
 module.exports = CreateOrdersSchema

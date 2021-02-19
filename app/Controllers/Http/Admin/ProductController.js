35893f8a23c7f192637c8395183ee94c3145b75c
Bladi Duran <bladi.duran@gmail.com>
2021-02-19 13:03:34 -0500

    redoing database migrations

--- app/Controllers/Http/Admin/ProductController.js
+++ app/Controllers/Http/Admin/ProductController.js
@@ -1,4 +1,5 @@
 'use strict'
+const Database = use('Database')
 
 class ProductController {
 
@@ -6,8 +7,12 @@ class ProductController {
     index({view}){
         return view.render('admin/products/all')
     }
-    store(){
-        return   
+    store({request, response}){
+        const post = request.post()
+        Database.raw(`INSERT INTO products (title, sku, material, description, brand_id, qty, size, user_id)
+        Values(${post.title}, ${post.sku}, ${post.material}, ${post.description}, 1, ${post.qty}, ${post.size}, 1)
+        `).then(response )
+        return `saved success`
     }
     create({view}){
         return view.render('admin/products/create') 

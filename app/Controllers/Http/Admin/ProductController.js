c1c9b2d2332e2e9e0bb2544680525fdbef23e946
Bladi Duran <bladi.duran@gmail.com>
2021-02-19 14:58:53 -0500

    migrations ran

--- app/Controllers/Http/Admin/ProductController.js
+++ app/Controllers/Http/Admin/ProductController.js
@@ -12,7 +12,7 @@ class ProductController {
         Database.raw(`INSERT INTO products (title, sku, material, description, brand_id, qty, size, user_id)
         Values(${post.title}, ${post.sku}, ${post.material}, ${post.description}, 1, ${post.qty}, ${post.size}, 1)
         `).then(response )
-        return `saved success`
+        
     }
     create({view}){
         return view.render('admin/products/create') 

c2e14cc9c4be505e70484f00eeceaea8135fb04a
Bladi Duran <bladi.duran@gmail.com>
2021-02-22 14:07:30 -0500

    added brands column

--- app/Controllers/Http/Admin/ProductController.js
+++ app/Controllers/Http/Admin/ProductController.js
@@ -3,13 +3,30 @@ const Database = use('Database')
 const sanitize = require('sqlstring');
 
 class ProductController {
-
-
-    index({view}){
-        return view.render('admin/products/all')
+    async index({view, request, response}){
+        try {
+            let allProducts = await Database.raw(`
+            SELECT products.id, products.title, products.sku,brands.title as brand, concat(users.f_name, ' ', users.l_name) as user, products.material, products.qty, products.size, products.user_id, products.created_at
+            FROM products
+            INNER JOIN brands
+            ON products.brand_id = brands.id
+            INNER JOIN users
+            ON products.user_id = users.id
+            ORDER BY created_at ASC
+        `)
+        allProducts = allProducts[0]
+        return view.render('admin/products/all', {allProducts})
+        } catch (error) {
+            console.log(error)
+            return response.redirect('back')
+            
+            // `<h1>There was an error</h1>
+            //  <h3>${error.sqlMessage}</h3>
+            //  `
+        }
+      
     }
     async store({request, response}){
-
         try {
             const post = request.post()
         await Database.raw(`INSERT INTO products (title, sku, material, description, brand_id, qty, size, user_id)
@@ -26,19 +43,19 @@ class ProductController {
             //  `
         }
     }
-    create({view}){
+    create({view, request, response}){
         return view.render('admin/products/create') 
     }
-    show({view}){
+    show({view, request, response}){
         return view.render('admin/products/show')   
     }
-    edit({view}){
+    edit({view, request, response}){
         return view.render('admin/products/edit')   
     }
-    update(){
+    update({request, response}){
         return   
     }
-    delete(){
+    delete({request, response}){
         return   
     }
 }
